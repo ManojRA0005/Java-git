e65fc0d1669da88ee5dc8b3fa5c6c4fac49396d3
ManojRA0005 <raavilamanoj@gmail.com>
2022-01-10 12:37:30 +0530

    Done

---
+++ src/gitex/gitpush.java
@@ -0,0 +1,12 @@
+package gitex;
+
+public class gitpush {
+	private void add() {
+		System.out.println("10");
+
+	}
+public static void main(String[] args) {
+	gitpush G = new gitpush();
+	G.add();
+}
+}
