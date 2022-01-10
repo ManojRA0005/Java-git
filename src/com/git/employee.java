378a3c262f7f43f657d62326a7147b31032b674c
ManojRA0005 <raavilamanoj@gmail.com>
2022-01-10 13:02:39 +0530

    task

---
+++ src/com/git/employee.java
@@ -0,0 +1,11 @@
+package com.git;
+
+public class employee {
+	private void add() {
+		System.out.println("100");
+	}
+public static void main(String[] args) {
+	employee E = new employee();
+	E.add();
+}
+}
